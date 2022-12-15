7db6228f2cb83b4788a27d464d475346566ebc72
CMRicardo <ricardocorrales84@gmail.com>
2022-12-15 16:45:30 -0600

    feat: :sparkles: Finished section 4

--- src/api/gitHubApi.ts
+++ src/api/gitHubApi.ts
@@ -2,5 +2,7 @@ import axios from "axios";
 
 export const githubApi = axios.create({
   baseURL: 'https://api.github.com/repos/facebook/react',
-  headers: {}
+  headers: {
+    Autorization: 'Bearer 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'
+  }
 })
