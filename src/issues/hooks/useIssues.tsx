e158bb8326bb80db00ba77cf87c3925ac9881dd5
CMRicardo <ricardocorrales84@gmail.com>
2022-12-19 15:20:30 -0600

    style: :sparkles: Add .prettierrc file

--- src/issues/hooks/useIssues.tsx
+++ src/issues/hooks/useIssues.tsx
@@ -1,14 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
 import { githubApi } from "../../api/gitHubApi";
+import { sleep } from "../../helpers/sleep";
 import { Issue } from "../interfaces";
 
 const getIssues = async (): Promise<Issue[]> => {
-  const { data } = await githubApi.get<Issue[]>("/issues", {
-    headers: {
-      Autorization: null,
-    },
-  });
-
+  await sleep();
+  const { data } = await githubApi.get<Issue[]>("/issues");
   return data;
 };
 
--- src/issues/views/ListView.tsx
+++ src/issues/views/ListView.tsx
@@ -1,22 +1,22 @@
-import { useState } from "react";
-import { LoadingIcon } from "../../shared/LoadingIcon";
-import { IssueList } from "../components/IssueList";
-import { LabelPicker } from "../components/LabelPicker";
-import { useIssues } from "../hooks";
+import { useState } from 'react'
+import { LoadingIcon } from '../../shared/LoadingIcon'
+import { IssueList } from '../components/IssueList'
+import { LabelPicker } from '../components/LabelPicker'
+import { useIssues } from '../hooks'
 
 export const ListView = () => {
-  const [selectedLabels, setSelectedLabels] = useState<string[]>([]);
-  const { issuesQuery } = useIssues();
+  const [selectedLabels, setSelectedLabels] = useState<string[]>([])
+  const { issuesQuery } = useIssues()
 
   const onLabelChanged = (labelName: string) => {
     selectedLabels.includes(labelName)
       ? setSelectedLabels(selectedLabels.filter((label) => label !== labelName))
-      : setSelectedLabels([...selectedLabels, labelName]);
-  };
+      : setSelectedLabels([...selectedLabels, labelName])
+  }
 
   return (
-    <div className="row mt-5">
-      <div className="col-8">
+    <div className='row mt-5'>
+      <div className='col-8'>
         {issuesQuery.isLoading ? (
           <LoadingIcon />
         ) : (
@@ -24,12 +24,12 @@ export const ListView = () => {
         )}
       </div>
 
-      <div className="col-4">
+      <div className='col-4'>
         <LabelPicker
           selectedLabels={selectedLabels}
           onChange={(labelName) => onLabelChanged(labelName)}
         />
       </div>
     </div>
-  );
-};
+  )
+}
